55f70ea6e7b60991fe1b622faba7feb39e1dfbf1
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-09-18 10:45:23 +0300

    Validating username and email

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -10,3 +10,20 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me!')
     submit = SubmitField('Login')
 
+class RegForm(FlaskForm):
+    email = StringField('Your Email Address', validators=[Required(),Email()])
+    username = StringField('Enter Your Username', validators=[Required()])
+    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError("Email is user assigned!!! Sorry")
+
+
+    def validate_username(self, data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError("Username is user assigned!!! Sorry")
+        
+    
