4015570d2e801a622f5128f3e3a873b52136a843
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-09-18 10:42:41 +0300

    Class Loginform

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,3 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import ValidationError, StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
+
+
+class LoginForm(FlaskForm):
+    username =StringField('Username',validators=[Required()])
+    password=StringField('Password',validators=[Required()])
+    remember = BooleanField('Remember Me!')
+    submit = SubmitField('Login')
+
