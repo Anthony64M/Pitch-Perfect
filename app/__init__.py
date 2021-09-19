1e41ba7404b0dbc138ada4cc5f444ee4615cefd6
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-09-19 13:52:03 +0300

    Changed after error in line 31

--- app/__init__.py
+++ app/__init__.py
@@ -28,7 +28,7 @@ def create_app(config_name):
 
     login_manager.init_app(app)
     db.init_app(app)
-    bootstrap_find_resource.init_app(app)
+    bootstrap.init_app(app)
     configure_uploads(app,photos)
     mail.init_app(app)
     
