4929798c033092db2855ab73e2f7e5d699c7d845
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-09-19 20:42:15 +0300

    Deployed to heroku

--- manage.py
+++ manage.py
@@ -3,12 +3,12 @@ from flask_migrate import Migrate, MigrateCommand
 from app import create_app,db
 from app.models import User
 
-app = create_app('production')
+app = create_app('ddevelopment')
 
 manager = Manager(app)
 migrate = Migrate(app,db)
 
-manager.add_command('db',MigrateCommand)
+manager.add_command('serevr',Server)
 manager.add_command('run',Server(use_debugger=True))
 
 
