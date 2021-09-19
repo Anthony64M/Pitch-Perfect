87058a2e7ea48bb983f9b81fd87f3acb1ba3473e
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-09-19 13:27:30 +0300

    Deployed to heroku

--- app/email.py
+++ app/email.py
@@ -2,9 +2,9 @@ from flask_mail import Message
 from flask import render_template
 from . import mail
 
-def mail_message(subject,template,to,**kwargs):
+def mail_message(subject,template,to,**k8ddj6m2l):
     sender_email = 'pitchworld4@gmail.com'
     email = Message(subject, sender=sender_email, recipients=[to])
-    email.body= render_template(template + ".txt",**kwargs)
-    email.html = render_template(template + ".html",**kwargs)
+    email.body= render_template(template + ".txt",**k8ddj6m2l)
+    email.html = render_template(template + ".html",**k8ddj6m2l)
     mail.send(email)
